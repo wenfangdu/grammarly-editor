bd4c367051d05dea609cf8cfdd9934f4bfcf31b7
Wenfang Du <duwenfangcn@163.com>
2021-02-19 13:37:05 +0800

    feat: improved caret position change detection

--- main.js
+++ main.js
@@ -6,20 +6,24 @@ const content = localStorage.getItem('content')
 content && (textarea.value = content)
 
 let caretPos = +localStorage.getItem('caretPos')
-
-if (Number.isNaN(caretPos)) {
-  caretPos = 0
-  localStorage.setItem('caretPos', caretPos)
-}
+Number.isNaN(caretPos) && (caretPos = 0)
 
 textarea.setSelectionRange(caretPos, caretPos)
 textarea.focus()
 
 textarea.oninput = () => localStorage.setItem('content', textarea.value)
 
-Array.of('onkeyup', 'onclick', 'oncontextmenu').forEach(listener => {
-  textarea[listener] = () =>
-    requestAnimationFrame(() =>
-      localStorage.setItem('caretPos', textarea.selectionStart),
-    )
-})
+const storeCaretPos = () =>
+  requestAnimationFrame(() =>
+    localStorage.setItem('caretPos', textarea.selectionStart),
+  )
+
+Array.of('oninput', 'onclick', 'oncontextmenu').forEach(
+  listener => (textarea[listener] = storeCaretPos),
+)
+
+textarea.onkeyup = ({ key }) => {
+  if (['Arrow', 'Page', 'Home', 'End'].some(type => key.startsWith(type))) {
+    storeCaretPos()
+  }
+}
