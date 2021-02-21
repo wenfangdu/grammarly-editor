f27a10b22d80b3d6fdf343c3602171a88a513795
Wenfang Du <duwenfangcn@163.com>
2021-02-21 16:31:06 +0800

    feat: use demo document if the content doesn't exist

--- main.js
+++ main.js
@@ -2,9 +2,22 @@
 
 const textarea = document.querySelector('textarea')
 
+const demoDoc = `The basics
+Mispellings and grammatical errors can effect your credibility. The same goes for misused commas, and other types of punctuation . Not only will Grammarly underline these issues in red, it will also showed you how to correctly write the sentence.
+But wait...there's more?
+Blue underlines mean a clarity issue has been spotted by Grammarly. You'll find suggestions that can possibly help you revise a wordy sentence in an effortless manner.
+
+Grammarly will also inspect your vocabulary carefully and suggest the best word with green underlines to make sure you don't have to analyze your writing too much.
+
+And when you wanna refine ur slang use and formality level, look for purple underlines. That's especially useful when writing for a broad audience ranging from businessmen to friends and family, don't you think?
+So what's next?
+Just follow these two steps:
+Sign up for a Grammarly account by clicking the "Sign Up" button at the bottom of this screen.
+Start writing!`
+
 const restoreContent = () => {
   const content = localStorage.getItem('content')
-  content && (textarea.value = content)
+  textarea.value = content ?? demoDoc
 }
 
 const restoreCaretPos = () => {
